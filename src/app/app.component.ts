2e311f8747566909cae6a75de30ef3e7102b9e6a
gsalva04 <gsalva04@gmail.com>
2024-05-29 11:30:44 +0200

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
+
+interface AutoCompleteCompleteEvent {
+    originalEvent: Event;
+    query: string;
+}
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    imports: [RouterOutlet, AutoCompleteModule, DropdownModule, FormsModule, ButtonModule],
+    standalone: true,
 })
 export class AppComponent {
-  title = 'TuEscuela';
+    items: any[] = [];
+
+    selectedItem: any;
+
+    suggestions: any[] = [];
+
+    search(event: AutoCompleteCompleteEvent) {
+        this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
+    }
 }
